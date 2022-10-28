2954c64d83a2d1e091d293b0e3e3a5a33560ee0d
unknown <013jubayer@gmail.com>
2022-10-28 14:28:05 +0600

    1st commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Components/Home/Home';
+
+function App() {
+  return (
+    <div >
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Carousel from './Carousel';
+import Navbar from './Navbar';
+
+const Home = () => {
+  return (
+    <div>
+    <Navbar></Navbar>
+    <Carousel></Carousel>
+    </div>
+  );
+};
+
+export default Home;
